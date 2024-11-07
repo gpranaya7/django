00f741f7ca3e8867ba7fa39b09c0d310d28674b6
gpranaya7 <144252746+gpranaya7@users.noreply.github.com>
2024-11-08 01:34:32 +0530

    Add files via upload

---
+++ studentsMF/app/models.py
@@ -0,0 +1,11 @@
+
+# Create your models here.
+from django.db import models
+from django.core.validators import *
+# Create your models here.
+class Student(models.Model):
+    sname=models.CharField(max_length=100,validators=[MinLengthValidator(5)])
+    sid=models.IntegerField(primary_key=True)
+    semail=models.EmailField()
+    def __str__(self):
+        return self.sname
